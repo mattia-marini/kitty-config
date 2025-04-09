5657243ea0a848e7c72f74ca8b7ffd89267f25b5
Mattia Marini <marmattia26@gmail.com>
2025-04-09 19:34:28 +0200

    Moved logger out of tab_bar

--- debug/logger.py
+++ debug/logger.py
@@ -1,6 +1,4 @@
 import os
-import inspect
-
 
 class Logger:
     def __init__(self, pipe_path: str = "/tmp/kitty_debug"):
@@ -22,24 +20,25 @@ class Logger:
             print(f"Could not write to pipe. Error: {e}")
 
     def inspect(self, obj: object):
-        for key, value in vars(logger).items():
-            self.log(f"{key}: {type(value).__name__}\n")
+        attributes = {}
+        for attr in dir(obj):
+            if attr.startswith('_'):
+                continue
+            try:
+                value = getattr(obj, attr)
+                attributes[attr] = type(value).__name__
+            except Exception as e:
+                attributes[attr] = f"<error: {e}>"
+    
+        formatted = "{\n" + "".join(f"    {k}: {v},\n" for k, v in attributes.items()) + "}"
+        self.log(formatted)
 
     def clear(self):
         self.log("\033[2J\033[H") #tput clear
         self.log("\033[3J\033[0;0H") #resets cursor
 
-
-
 # Example usage:
 logger = Logger("/tmp/kitty_debug")
-# logger.log("Hello from Logger!\n")
 logger.inspect(logger)
 logger.clear()
 logger.inspect(logger)
-
-
-# for key, value in vars(logger).items():
-#     print(f"{key}: {type(value).__name__}")
-
-# print(dir(logger))
--- tab_bar.py
+++ tab_bar.py
@@ -8,44 +8,6 @@ from kitty.tab_bar import (
     draw_title
 )
 
-class Logger:
-    def __init__(self, pipe_path: str = "/tmp/kitty_debug"):
-        self.pipe_path = pipe_path
-        if not os.path.exists(self.pipe_path):
-            os.mkfifo(self.pipe_path)
-        self.clear()
-        self.log("Logger initialized\n")
-
-
-
-    def log(self, msg: str):
-        try:
-            fd = os.open(self.pipe_path, os.O_WRONLY | os.O_NONBLOCK)
-            with os.fdopen(fd, "w") as pipe:
-                pipe.write(msg)
-                pipe.flush()
-        except OSError as e:
-            print(f"Could not write to pipe. Error: {e}")
-
-    def inspect(self, obj: object):
-        attributes = {}
-        for attr in dir(obj):
-            if attr.startswith('_'):
-                continue
-            try:
-                value = getattr(obj, attr)
-                attributes[attr] = type(value).__name__
-            except Exception as e:
-                attributes[attr] = f"<error: {e}>"
-    
-        formatted = "{\n" + "".join(f"    {k}: {v},\n" for k, v in attributes.items()) + "}"
-        self.log(formatted)
-
-    def clear(self):
-        self.log("\033[2J\033[H") #tput clear
-        self.log("\033[3J\033[0;0H") #resets cursor
-
-logger = Logger("/tmp/kitty_debug")
 
 def as_hex(color: Color):
     return "{:02X}{:02X}{:02X}".format(color.red, color.green, color.blue)
